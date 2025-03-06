4ea86c5c01534e8653b2f1a634cf771587044c8b
𝙰𝚂𝙷𝙾𝙺𝚇𝙶𝙷𝙾𝚂𝚃 <106798009+AshokChakravarthy666@users.noreply.github.com>
2025-03-06 19:36:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,24 +4,24 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/SHProSearchbot>SHProSearchbot</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/PS_MOVIES11>PS</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: KOYEB
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- SHProSearchbot-AutoFilterBot is a open source project. 
+- ಬಡವ ರಾಸ್ಕಲ್ 🫧 -AutoFilterBot is a open source project. 
 - Source - 🔐 
 
 <b>DEVS:</b>
-- <a href=https://t.me/SHProSearchbot>SHProSearchbot</a>"""
+- <a href=https://t.me/PS_MOVIES11>PS</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and ಬಡವ ರಾಸ್ಕಲ್ 🫧 will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. SHProSearchbot-AutoFilterBot should have admin privillage.
+1. ಬಡವ ರಾಸ್ಕಲ್ 🫧 -AutoFilterBot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,15 +32,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- SHProSearchbot-AutoFilterBot Supports both url and alert inline buttons.
+- ಬಡವ ರಾಸ್ಕಲ್ 🫧-AutoFilterBot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. SHProSearchbot-AutoFilterBot supports buttons with any telegram media type.
+2. ಬಡವ ರಾಸ್ಕಲ್ 🫧-AutoFilterBot supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/SHProSearchbot)</code>
+<code>[Button Text](buttonurl:https://t.me/PS_MOVIES11)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -67,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of ಬಡವ ರಾಸ್ಕಲ್ 🫧
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
